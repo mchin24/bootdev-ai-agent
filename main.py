f7bddb65f03588b0495d36638047180ae6bf0a40
mchin24 <mchin24@gmail.com>
2025-12-22 09:21:26 -0500

    Defined a tool and told the agent about it

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 from prompts import system_prompt
+from call_function import available_functions
 
 load_dotenv()
 api_key = os.getenv('GEMINI_API_KEY')
@@ -24,7 +25,9 @@ def main():
     response = client.models.generate_content(
         model="gemini-2.5-flash",
         contents= messages,
-        config=types.GenerateContentConfig(system_instruction = system_prompt)
+        config=types.GenerateContentConfig(
+            tools=[available_functions],
+            system_instruction=system_prompt)
     )
 
     # Output response
@@ -35,6 +38,11 @@ def main():
         print(f"User prompt: {args.user_prompt}")
         print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
         print(f"Response tokens:{response.usage_metadata.candidates_token_count}")
+
+    if response.function_calls is not None:
+        for function_call in response.function_calls:
+            print(f"Calling function: {function_call.name}({function_call.args})")
+            return None
 
     print(response.text)
 
