9f8c3a61d528516a2526c335eb844d9a24a786fe
mchin24 <mchin24@gmail.com>
2025-12-23 00:55:21 -0500

    Actually running tools on behalf of the agent

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import os
 import argparse
+from functions.get_file_content import get_file_content
+from functions.get_files_info import get_files_info
+from functions.run_python_file import run_python_file
+from functions.write_file import write_file
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
@@ -10,6 +14,45 @@ load_dotenv()
 api_key = os.getenv('GEMINI_API_KEY')
 if api_key == None:
     raise RuntimeError("GEMINI_API_KEY is not set in the environment variables.")
+
+def call_function(function_call, verbose=False):
+    if verbose:
+        print(f"Calling function: {function_call.name}({function_call.args})")
+    else:
+        print(f" - Calling function: {function_call.name}")
+
+    match function_call.name:
+        case "get_file_content":
+            function_result = get_file_content('calculator', **function_call.args)
+        case "get_files_info":
+            function_result = get_files_info('calculator', **function_call.args)
+        case "run_python_file":
+            function_result = run_python_file('calculator', **function_call.args)
+        case "write_file":
+            function_result = write_file('calculator', **function_call.args)
+        case _: 
+            function_result = None
+
+    if function_result is None:
+        return types.Content(
+            role="tool",
+            parts = [
+                types.Part.from_function_response(
+                    name=function_call.name,
+                    response={"error": f"Unknown function: {function_call.name}"}
+                )
+            ]
+        )
+    else:
+        return types.Content(
+            role="tool",
+            parts = [
+                types.Part.from_function_response(
+                    name=function_call.name,
+                    response={"result" : function_result}
+                )
+            ]
+        )
 
 def main():
     parser = argparse.ArgumentParser(description="Chatbot")
@@ -39,9 +82,18 @@ def main():
         print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
         print(f"Response tokens:{response.usage_metadata.candidates_token_count}")
 
+    responses = []
     if response.function_calls is not None:
         for function_call in response.function_calls:
-            print(f"Calling function: {function_call.name}({function_call.args})")
+            function_response = call_function(function_call, args.verbose)
+
+            if not function_response.parts[0].function_response.response:
+                raise RuntimeError("Function response is empty.")
+            
+            responses.append(function_response.parts[0])
+
+            if args.verbose:
+                print(f"-> {function_response.parts[0].function_response.response}")
             return None
 
     print(response.text)
